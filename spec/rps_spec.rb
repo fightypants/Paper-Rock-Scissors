656ed707014c503be2e435aa7ce422b6cdd5e1f0
kha le & cory spitzer <student@epicodus.com>
2015-04-22 16:24:54 -0700

    Fix rock beats scissors test

---
+++ spec/rps_spec.rb
@@ -0,0 +1,10 @@
+require('rspec')
+require('./rps_app')
+
+
+
+describe('String#beats?') do
+  it("returns true if rock is the object and scissors is the argument") do
+    expect("rock".beats?("scissors")).to(eq(true))
+  end
+end
